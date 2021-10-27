24bb26f8e25536233d607e5abdc76a0e7e57bfe8
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-10-27 13:39:53 +0200

    Ajout des images à transformer

--- killian.py
+++ killian.py
@@ -1,3 +1,10 @@
 import cv2
 
-image = cv2.imread('C:/Users/N/Desktop/Test.jpg')
+for i in range(0, 2):
+    image = cv2.imread("image0.jpeg")
+    gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Original image', image)
+    cv2.imshow('Gray image', gris)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
