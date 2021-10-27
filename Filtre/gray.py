06a1dd04c6cd590acab61c025ac5a7409c6a61e7
EddyMuzaffar <eddy.muzaffar@outlook.com>
2021-10-27 15:54:39 +0200

    correction de multiple erreur

---
+++ Filtre/gray.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy
+
+def grey(image):
+    try:
+        img = cv2.imread(image)
+
+
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("Gaussian Smoothing",gray)
+    except Exception as e:
+        print(f"L'action n'as pas pu etre effectuer {e}")
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
