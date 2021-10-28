15fbe824cb9996916598437d5cfafb1b10bb9456
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-10-28 14:49:47 +0200

    correction de bugs apres le merge

--- Filtre/flou.py
+++ Filtre/flou.py
@@ -2,12 +2,13 @@ import cv2
 import numpy
 
 
-def flou(image):
+def floutage(image):
     try:
         img = cv2.imread(image)
         dst = cv2.GaussianBlur(img,(51,51),51)
         cv2.imshow("Image Flouter",numpy.hstack((img, dst)))
-        cv2.imwrite('image/imagemodifier.jpg', dst)
+        for j in range(3):
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
     except Exception as e:
         print(f"L'image n'as pas pu etre effectuer{e}")
     cv2.waitKey(0)
