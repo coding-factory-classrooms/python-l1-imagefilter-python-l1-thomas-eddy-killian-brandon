25a5c95e4d2691926ec48b137c0e7a150c44268f
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-10-28 16:38:45 +0200

    docstring de toutes les fonctions.

--- log.py
+++ log.py
@@ -7,10 +7,21 @@ date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 log_file ='imagefilter.log'
 
 def log(message):
+
+    """
+    log(fonction) écrit dans les logs le message en paramètre avec la date du jour.
+    :param message: str - message à écrire dans les logs.
+    """
+
     with open(log_file,'a') as f:
         f.write(date_time +" "+ message+"\n")
 
 
 def clear_log():
+
+    """
+    clear_log nettoie les logs encore plus rapidement que swiffer.
+    """
+
     with open(log_file,'w'):
         pass
