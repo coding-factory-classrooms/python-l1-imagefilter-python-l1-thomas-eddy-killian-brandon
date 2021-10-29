2b4aa1767eb197a34e5f2cba30e49df8ff801243
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-10-29 10:04:59 +0200

    tentative de multi-filtre (spoiler alert: ca marche pas)

--- cli.py
+++ cli.py
@@ -14,56 +14,55 @@ def multi_filtre(arg):
     :param arg: filtres à utiliser.
     """
     j = 0
-    while j < 3:
-        for i in images:
-            if arg == "gris":
-                dst = filtre.gris.noir_blanc(i)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "flou":
-                dst = filtre.flou.floutage(i)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "dilate":
-                dst = filtre.dilatation.dilater(i)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "gris|flou":
-                dst = filtre.gris.noir_blanc(i)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "flou|dilate":
-                dst = filtre.flou.floutage(i)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "gris|dilate":
-                dst = filtre.gris.noir_blanc(i)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "gris|flou|dilate":
-                dst = filtre.gris.noir_blanc(i)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-        for k in filtre_image:
-            if arg == "gris|flou":
-                dst = filtre.gris.noir_blanc(k)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "flou|dilate":
-                dst = filtre.flou.floutage(k)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "gris|dilate":
-                dst = filtre.gris.noir_blanc(k)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-            elif arg == "gris|flou|dilate":
-                dst = filtre.gris.noir_blanc(k)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-        for l in filtre_image:
-            if arg == "gris|flou|dilate":
-                dst = filtre.gris.noir_blanc(l)
-                cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
-                filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
-        j += 1
+    for i in images:
+        if arg == "gris":
+            dst = filtre.gris.noir_blanc(i)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "flou":
+            dst = filtre.flou.floutage(i)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "dilate":
+            dst = filtre.dilatation.dilater(i)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "gris|flou":
+            dst = filtre.gris.noir_blanc(i)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "flou|dilate":
+            dst = filtre.flou.floutage(i)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "gris|dilate":
+            dst = filtre.gris.noir_blanc(i)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "gris|flou|dilate":
+            dst = filtre.gris.noir_blanc(i)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+    for k in filtre_image:
+        if arg == "gris|flou":
+            dst = filtre.gris.noir_blanc(k)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "flou|dilate":
+            dst = filtre.flou.floutage(k)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "gris|dilate":
+            dst = filtre.gris.noir_blanc(k)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+        elif arg == "gris|flou|dilate":
+            dst = filtre.gris.noir_blanc(k)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+    for l in filtre_image:
+        if arg == "gris|flou|dilate":
+            dst = filtre.gris.noir_blanc(l)
+            cv2.imwrite('filtre_image/imagemodifier' + str(j) + '.jpg', dst)
+            filtre_image.append('filtre_image/imagemodifier' + str(j) + '.jpg')
+    j += 1
